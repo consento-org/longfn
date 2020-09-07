a84c202dbb2b2dff3ab800121103224befd2bd58
Martin Heidegger <martin.heidegger@gmail.com>
2020-09-08 02:06:52 +0900

    test: performance: Improved performance tests to run the tests more than once and return more stable results

--- test/perf.js
+++ test/perf.js
@@ -5,39 +5,33 @@ const longfn = require('..')
 const longjs = require('long')
 const sshuffle = require('secure-shuffle')
 
-// Creating a 32MB buffer to fill
-const buf = Buffer.alloc(1024 * 1024 * 64)
-const result = new Uint32Array(buf.buffer)
-
 function fnImpl (result) {
   let a = longfn.fromInt(1234141)
   let b = longfn.fromInt(4141241)
   const FOUR = longfn.fromInt(4)
   const FIFTY_THREE = longfn.fromInt(53)
   const fnMax = longfn.fromInt(12341411234141)
-  let count = 0
   return () => {
-    for (let i = 0; i < result.length; i+=2) {
+    for (let i = 0; i < result.length; i += 2) {
       longfn.mul(longfn.xor(a, b, a), FOUR, a)
       if (longfn.ge(a, fnMax)) {
-        count += 1
         longfn.div(a, FIFTY_THREE, a)
       }
       const tmp = a
       a = b
       b = tmp
       result[i] = a.low
-      result[i+1] = a.high
+      result[i + 1] = a.high
     }
   }
 }
 
-function jsImpl (arr) {
+function jsImpl (result) {
   const jsMax = longjs.fromInt(12341411234141)
   let c = longjs.fromInt(1234141)
   let d = longjs.fromInt(4141241)
   return () => {
-    for (let i = 0; i < result.length; i+=2) {
+    for (let i = 0; i < result.length; i += 2) {
       let tmp = c.xor(d).mul(4)
       if (tmp.gte(jsMax)) {
         tmp = tmp.div(53)
@@ -45,7 +39,7 @@ function jsImpl (arr) {
       c = d
       d = tmp
       result[i] = c.low
-      result[i+1] = c.high
+      result[i + 1] = c.high
     }
   }
 }
@@ -55,18 +49,28 @@ const impls = {
   fn: fnImpl
 }
 
-let firstResult
-for (const impl of sshuffle(Object.keys(impls))) {
-  const buf = Buffer.alloc(1024 * 1024 * 32) // 32 MB
-  const run = impls[impl](new Uint32Array(buf))
+function exec (name, size, ignore) {
+  const buf = Buffer.alloc(size) // 32 MB
+  const run = impls[name](new Uint32Array(buf))
   const start = Date.now()
   run()
   const end = Date.now()
-  console.log(`${impl}: ${end-start}ms`)
-  if (firstResult === undefined) {
-    firstResult = buf
-  } else {
-    console.log(`Same output? ${firstResult.equals(buf)}`)
+  if (!ignore) {
+    results[name] += (end - start)
   }
 }
 
+const results = {}
+for (const name in impls) {
+  results[name] = 0
+  exec(name, 1024, true)
+}
+for (let i = 0; i < 3; i++) {
+  for (const name of sshuffle(Object.keys(impls))) {
+    exec(name, 1024 * 1024 * 32)
+  }
+}
+
+for (const name in impls) {
+  console.log(`${name}: ${results[name]}ms`)
+}
